91f00474d94f75852bbdb69d7402f02cf0d30b22
Etiketaa <francoparedes1992@gmail.com>
2025-09-17 22:24:43 -0300

    weatherrr

--- static/script.js
+++ static/script.js
@@ -79,7 +79,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const taskForm = document.getElementById('task-form');
     const taskInput = document.getElementById('task-input');
-    const taskColumns = document.querySelectorAll('.task-list-column');
 
     const settingsBtn = document.getElementById('settings-btn');
     const settingsModal = document.getElementById('settings-modal');
